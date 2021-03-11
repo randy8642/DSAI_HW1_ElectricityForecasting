9f48fec4fad48ea01f5ff229043e11b1e9b6aa1a
randy8642 <randy8642@gmail.com>
2021-03-11 20:54:47 +0800

    Update preProcess.py

--- preProcess.py
+++ preProcess.py
@@ -11,13 +11,8 @@ df['year'] = [datetime.strptime(str(a), "%Y%m%d").year for a in df['日期'].val
 df = df[['year','weekcount','daycount','淨尖峰供電能力(MW)','尖峰負載(MW)','備轉容量(MW)']]
 # df = df.rename(columns={'淨尖峰供電能力(MW)':'supply','尖峰負載(MW)':'load','備轉容量(MW)':'remain'})
 
-import matplotlib.pyplot as plt
 
-plt.title('備轉容量(MW)')
-plt.plot(df[df['year']==2019]['備轉容量(MW)'].values,label='2019')
-plt.plot(df[df['year']==2020]['備轉容量(MW)'].values,label='2020')
-plt.legend()
-plt.show()
 
-exit()
-df.to_csv('./data/training_data.csv')
+
+# df.to_csv('./data/training_data.csv')
+
