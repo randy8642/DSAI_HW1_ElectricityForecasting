240d462027bfce6de1af32f926db3f25f92279bd
randy8642 <randy8642@gmail.com>
2021-03-12 14:25:34 +0800

    20210312

--- app.py
+++ app.py
@@ -22,84 +22,25 @@ def main():
                         help='output file name')
     args = parser.parse_args()
 
-    # prepare dataset
-    # df = pd.read_csv(args.training)
-    data = np.load('./data/trainData.npy')
-    train_set = torch.from_numpy(data[:700]).type(torch.FloatTensor)
-    trainLoader = torch.utils.data.DataLoader(
-        train_set, batch_size=BATCH_SIZE, shuffle=True)
-    test_set = torch.from_numpy(data[700:]).type(torch.FloatTensor)
-    testLoader = torch.utils.data.DataLoader(
-        test_set, batch_size=BATCH_SIZE, shuffle=False)
 
-    # model & opt & lossfunc
-    model = ElectricityForecastingModel()
-    optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
-    loss_function = torch.nn.MSELoss()
-
-    # TRAIN
-    train(model=model, dataloader=trainLoader,
-          optimizer=optimizer, lossFunction=loss_function)
-
-    # PREDICT
-    a = predict(model=model, dataloader=testLoader)
-
-
-def train(model, dataloader, optimizer, lossFunction):
-    for epoch in range(EPOCH):
-        epoch += 1
-
-        model.train()
-
-        for n, data in enumerate(dataloader):
-            
-
-            
-
-            pred = model()
-
-            # Backward
-            optimizer.zero_grad()
-
-            loss = lossFunction(pred, (supply - load)/10000)
-
-            loss.backward()
-
-            optimizer.step()
-
-        with torch.no_grad():
-            print(loss.item())
-
-    pass
-
-
-def predict(model, dataloader):
-    model.eval()
-
-    pred = np.zeros([0, 1])
-    target = np.zeros([0, 1])
-    for n, data in enumerate(dataloader):
-        year, weekcount, daycount, supply, load = data[:,
-                                                       0], data[:, 1], data[:, 2], data[:, 3], data[:, 4]
-
-        weekcount = weekcount.type(torch.LongTensor)
-        daycount = daycount.type(torch.LongTensor)
-
-        out = model(year, weekcount, daycount)
-
-        out = out.detach().numpy().reshape(-1, 1)
-
-        pred = np.concatenate((pred, out), axis=0)
-        target = np.concatenate(
-            (target, ((supply - load)/10000).reshape(-1, 1)), axis=0)
-
-    print(pred.shape)
-    print(target.shape)
-
-    import matplotlib.pyplot as plt
-    plt.plot(pred)
-    plt.plot(target)
-    plt.show()
+    #　TRAIN
+    import numpy as np
+    from sklearn.linear_model import SGDRegressor
+    from sklearn.pipeline import make_pipeline
+    from sklearn.preprocessing import StandardScaler
+    d = np.load('./data/trainData.npy')
+    x = d[:-5,:3]
+    y = d[:-5,-1:]
+    reg = make_pipeline(StandardScaler(),
+                    SGDRegressor(max_iter=1000, tol=1e-3))
+    reg.fit(x, y)
+    # r2
+    print(reg.score(x, y))
+    
+    # predict
+    print(np.round(reg.predict(d[-5:,:3])))
+    print(d[-5:,-1:].flatten())
+    
 
 
 if __name__ == '__main__':
--- preProcess.py
+++ preProcess.py
@@ -3,7 +3,33 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from datetime import datetime
 
+from pandas.core.algorithms import value_counts
 
+df = pd.read_csv('./data/假日表.csv')
+df['weekend'] = ((df['Day of the week'] == '週六') | (df['Day of the week'] ==  '週日'))
+df['weekday'] = [not i for i in df['weekend'].values]
+a = []
+for i in df['Day of the week'].values:
+    if i == '週一':
+        a.append(1)
+    elif i == '週二':
+        a.append(2)
+    elif i == '週三':
+        a.append(3)
+    elif i == '週四':
+        a.append(4)
+    elif i == '週五':
+        a.append(5)
+    elif i == '週六':
+        a.append(6)
+    elif i == '週日':
+        a.append(7)
+print(len(a))
+print(df['Day of the week'].shape)
+df['Day of the week'] = a
+df.to_csv('a.csv')
+print(df)
+exit()
 df = pd.read_csv('./data/台灣電力公司_過去電力供需資訊.csv')
 
 df['weekcount'] = [datetime.strptime(str(a), "%Y%m%d").isocalendar()[
@@ -19,14 +45,7 @@ df = df.rename(columns={'淨尖峰供電能力(MW)': 'supply',
                         '尖峰負載(MW)': 'load', '備轉容量(MW)': 'remain'})
 
 
-# df.to_csv('./data/training_data.csv')
-
-
 a = df[['year', 'weekcount', 'daycount', 'remain']]
-d = np.zeros([0, 7, 4])
-for row in range(a.values.shape[0]-6):
-    s = a.values[row:row+7, :].reshape(1, 7, 4)
+d = a.to_numpy()
 
-    d = np.concatenate((d, s), axis=0)
-print(d.shape)
 np.save('./data/trainData.npy', d)
