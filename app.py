70033abc4b2f2e19fe2d3a2f1d7da615cb9764fc
randy8642 <randy8642@gmail.com>
2021-03-12 15:23:11 +0800

    20210312

--- app.py
+++ app.py
@@ -20,25 +20,27 @@ def main():
                         help='output file name')
     args = parser.parse_args()
 
-
     #　TRAIN
     import numpy as np
     from sklearn.neural_network import MLPRegressor
     d = np.load('./data/trainData.npy')
-    n = 50
-    x = d[:-n,:-1]
-    y = d[:-n,-1:]
+    n = 20
+    x = d[:-n, :-1]
+    y = d[:-n, -1:]
     reg = MLPRegressor(random_state=1, max_iter=1000)
     reg.fit(x, y)
     # r2
     print(reg.score(x, y))
-    
+
     # predict
-    print(round(reg.predict(d[-n:,:-1])))
-    print(d[-n:,-1:].flatten())
-    
+    test_x = d[-n:, :-1]
+    test_y = d[-n:, -1:].flatten()
+    pred = reg.predict(test_x)
+
+    print(test_y)
+    print(pred)
     from sklearn.metrics import r2_score
-    print(r2_score(d[-n:,-1:].flatten(),(reg.predict(d[-n:,:-1]))))
+    print(r2_score(test_y, pred))
 
 
 if __name__ == '__main__':
