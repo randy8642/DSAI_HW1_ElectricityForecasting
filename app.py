29bad70c1bd61e5c4d775e02b9b672d088daafb5
randy8642 <randy8642@gmail.com>
2021-03-14 12:40:30 +0800

    20210312

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ import torch.nn as nn
 import torch.utils.data as data
 import torch.optim as optim
 import sklearn.metrics as metrics
+import matplotlib.pyplot as plt
 
-EPOCH = 50
-BATCH_SIZE = 32
-LEARNING_RATE = 1e-4
+EPOCH = 2000
+LEARNING_RATE = 1e-1
 
 
 def main():
@@ -28,69 +28,35 @@ def main():
     d = np.load('./data/trainData.npz')
     train_x = torch.from_numpy(d['train_x'][:-7]).type(torch.FloatTensor)
     train_y = torch.from_numpy(d['train_y'][:-7]).type(torch.FloatTensor)
-    trainDataSet = data.TensorDataset(train_x, train_y)
-    trainLoader = data.DataLoader(
-        trainDataSet, batch_size=BATCH_SIZE, shuffle=True)
 
-    model = m()
-    optimizer = optim.Adam(model.parameters(), lr=0.01)
+
+    model = nn.Sequential(
+        nn.Linear(4, 1),
+        nn.Flatten(0, 1)
+    )
+    optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
     lossFunction = nn.MSELoss()
 
     model.train()
     for epoch in range(EPOCH):
         print(f'epoch {epoch+1}/{EPOCH}', end='\r')
-        for n, (x, y) in enumerate(trainLoader):
 
-            pred = model(x)
+        pred = model(train_x)
 
-            optimizer.zero_grad()
-            loss = lossFunction(pred, y)
-            loss.backward()
-            optimizer.step()
+        optimizer.zero_grad()
+        loss = lossFunction(pred, train_y)
+        loss.backward()
+        optimizer.step()
         with torch.no_grad():
             print(f'loss {loss.item()}')
     print('')
+    
 
     # TEST
-    test_x = torch.from_numpy(d['train_x'][-7:]).type(torch.FloatTensor)
-    test_y = torch.from_numpy(d['train_y'][-7:]).type(torch.FloatTensor)
-    testDataSet = data.TensorDataset(test_x, test_y)
-    testLoader = data.DataLoader(
-        testDataSet, batch_size=BATCH_SIZE, shuffle=False)
-    with torch.no_grad():
-        model.eval()
-        pred = torch.zeros([0, 1])
-        for n, (x, y) in enumerate(trainLoader):
-            out = model(x)
-            pred = torch.cat((out, pred), dim=0)
-
-        r2 = metrics.r2_score(y_true=train_y.numpy(), y_pred=pred.numpy())
-        print(test_y.numpy().flatten())
-        print(pred.numpy().flatten())
-        print(r2)
-        import matplotlib.pyplot as plt
-        plt.plot(train_y)
-        plt.plot(pred)
-        plt.show()
-
-
-class m(nn.Module):
-    def __init__(self):
-        super().__init__()
-
-        self.layers = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(42, 100),
-            nn.ReLU(),
-            nn.Linear(100, 100),
-            nn.ReLU(),
-            nn.Linear(100, 1),
-        )
-
-    def forward(self, x):
-        
-        x = self.layers(x)
-        return x
+    y = model(train_x).detach().numpy()
+    plt.plot(train_x[:, 0], train_y)
+    plt.plot(train_x[:, 0], y)
+    plt.show()
 
 
 if __name__ == '__main__':
