93a92c9d4c94ee6eea3149c2c5080305a87fc2d1
randy8642 <randy8642@gmail.com>
2021-03-16 13:46:21 +0800

    plot

--- plot.py
+++ plot.py
@@ -1,36 +1,46 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
-# READ
-
-df = pd.read_csv('./data/每日備轉容量.csv')
-
-
-def filter(data):
-    fft = np.fft.fft(data)
-    fftf = np.fft.fftfreq(data.shape[0])
-    # fft[abs(fftf) > 0.035] = 0
-    fft[abs(fftf) < 0.03] = 0
-    return np.fft.ifft(fft).real
+import matplotlib.font_manager as fm
+fontPath = './data/NotoSansTC-Regular.otf'
+font_legend = fm.FontProperties(fname=fontPath, size=16)
+font_ticks = fm.FontProperties(fname=fontPath, size=11)
+font_title = fm.FontProperties(fname=fontPath, size=20)
 
 
-d = filter(df['備轉容量(MW)'])
-# d = (df['備轉容量(MW)']).values
+df = pd.read_csv('./data/electricity_data.csv')
+
+# df = df[['Day of the week', 'work', '備轉容量(MW)','瞬時尖峰負載(MW)','淨尖峰供電能力(MW)']]
+# df['work'] = [1 if i else 0 for i in df['work']]
 
 
-plt.plot(d)
+def splitWeek(data):
+    week = np.zeros([0, 7])
+    for i in np.arange(0, data.shape[0]-7, 7):
+        week = np.concatenate((week, data[i:i+7].reshape(1, -1)), axis=0)
+    return week
+
+
+d = splitWeek(df['備轉容量率(%)'].values)
+
+# 移除平均
+d = d - np.repeat(np.mean(d, axis=1, keepdims=True), 7, axis=1)
+
+# d = df['備轉容量率(%)']
+
+plt.plot(d.T, alpha=0.3)
+
+plt.xticks(ticks=np.arange(7), labels=['Mon.', 'Tue.', 'Wen.', 'Thur.', 'Fri.', 'Sat.', 'Sun.'], fontproperties=font_ticks)
+
+
+# plt.xticks(ticks=np.arange(804)[::20], labels=df['date'].values[::20], rotation=60, fontproperties=font_ticks)
+plt.title('備轉容量率(%)[當週] - 平均[當週]',fontproperties=font_title)
+plt.grid()
+# plt.legend(loc=1, prop=font_legend)
 plt.show()
 
-exit()
-aaa = np.zeros([0,7])
-for n in np.arange(0, 777, 7):
-    aaa = np.concatenate((aaa, (d[n+7:n+7+7]-d[n:n+7].mean()).reshape(1,7)), axis=0)
-print(aaa)
 
 
-plt.plot(aaa.T)
-plt.xticks(ticks=range(7), labels=[
-           'Tue.', 'Wen.', 'Thur.', 'Fri.', 'Sat.', 'Sun.', 'Mon.'])
-plt.suptitle('Remain(MW)')
-plt.show()
+
+
+
--- preProcess.py
+++ preProcess.py
@@ -2,22 +2,17 @@ import numpy as np
 import pandas as pd
 
 
-df = pd.read_csv('./data/每日備轉容量.csv')
+df = pd.read_csv('./data/electricity_data.csv')
 
-df = df[['Day of the week', 'work', '備轉容量(MW)']]
+df = df[['Day of the week', 'work', '備轉容量(MW)','瞬時尖峰負載(MW)','淨尖峰供電能力(MW)']]
 df['work'] = [1 if i else 0 for i in df['work']]
 print(df)
-# def filter(data):
-#     fft = np.fft.fft(data)
-#     fftf = np.fft.fftfreq(data.shape[0])
-#     fft[abs(fftf) < 0.03] = 0
-#     return np.fft.ifft(fft).real
-# df['備轉容量(MW)'] = filter(df['備轉容量(MW)'])
 
+exit()
 train_x = np.zeros([0, 3])
 train_y = np.zeros([0, 7])
 a = df.to_numpy()
-# n=6時為周二(2019/01/08)
+# n=0時為周二(2019/01/01)
 for n in range(6, a.shape[0]-8, 7):
     train_x = np.concatenate((train_x, a[n, :].reshape(1, 3)), axis=0)
     train_y = np.concatenate(
