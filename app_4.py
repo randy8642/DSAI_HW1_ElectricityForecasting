8bd53158ea635e69c67b4da4751ffa1877690485
Abner0627 <n16090065@gs.ncku.edu.tw>
2021-03-16 13:36:59 +0800

    3/16

---
+++ app_4.py
@@ -0,0 +1,84 @@
+  
+# You should not modify this part, but additional arguments are allowed.
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-T','--training',
+                   default='training_data2.csv',
+                   help='input training data file name')
+
+parser.add_argument('--output',
+                    default='submission.csv',
+                    help='output file name')
+args = parser.parse_args()
+
+#%%
+# The following part is an example.
+# You can modify it at will.
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import time
+from preProcess_3 import _PreProcess
+
+#%%
+tStart = time.time()
+
+#%% Functions
+def _RMSE(pred, val):
+    mse = ((pred - val)**2).mean()
+    rmse = np.sqrt(mse)
+    return rmse
+
+#%% Get Data
+TRA_data, TRA_label, VAL_data, VAL_label, TES_data = _PreProcess(args.training)
+
+#%% Train
+# model
+model = LinearRegression(fit_intercept=True)
+W = []
+B = []
+# Train
+for i in range(7):
+    data = TRA_data[str(i)]
+    label = TRA_label[str(i)]
+    bz = data.shape[0]
+    data = data.reshape(bz, -1)
+    
+    model.fit(data, label)
+    W.append(model.coef_)
+    B.append(model.intercept_)
+    
+# Val.
+P = []
+L = []
+for j in range(7):
+    data = VAL_data[str(j)].reshape(-1)
+    pred = np.dot(data, W[j]) + B[j]
+    P.append(pred)
+    L.append(VAL_label[str(j)])
+    
+VAL_RMSE = _RMSE(np.array(P), np.array(L))
+print("VAL_RMSE >>", VAL_RMSE)
+    
+'''
+# Val
+val_pred = np.dot(VAL_data.reshape(7, -1), w) + b
+VAL_RMSE = _RMSE(val_pred, VAL_label)
+print("VAL_RMSE >>", VAL_RMSE)
+
+#%% Test
+PRED = np.dot(TES_data.reshape(7, -1), w) + b
+
+Date = []
+for i in range(7):
+    Date.append(20210323+i)
+Value = PRED.squeeze()
+
+diction = {"Date": Date,
+           "Value": Value
+           }
+select_df = pd.DataFrame(diction)
+select_df.to_csv(args.output,index=0,header=0)
+'''
+
--- preProcess_3.py
+++ preProcess_3.py
@@ -5,11 +5,10 @@ import os
 
 #%% Arg.
 def _PreProcess(fname):
-
     #%% Path
     fpath = "./data"
-    #fname = "training_data.csv"
-    H = 30
+    #fname = "training_data2.csv"
+    H = 10
     #%% Functions
     def _pack(data, H):
         D = []
@@ -22,43 +21,64 @@ def _PreProcess(fname):
         D = np.array(D)
         return D
     
-    def _norData(data):
-        y = np.copy(data)
-        min_MW = np.min(data, axis=0)
-        max_MW = np.max(data, axis=0)
-        y = (y-min_MW) / (max_MW-min_MW)
-    
-        return y
-    
-    def _norLabel(label):
-        y = np.copy(label)
-        mu = np.mean(y)
-        std = np.std(y)
-        y = (label-mu) / std
-        return y, mu, std
-    
     #%% Read csv
     Data = np.array(pd.read_csv(os.path.join(fpath, fname)))
-    train_un_data = Data[:769, 4:]
-    train_un_label = Data[49:789, 4]
-    val_un_data = Data[734:770, 4:]
-    val_un_label = Data[789:796, 4]
-    test_un_data = Data[762:798, 4:]
     
-    train_nor_data = _norData(train_un_data)
-    val_nor_data = _norData(val_un_data)
-    test_nor_data = _norData(test_un_data)
+    # Data
+    train_data = {
+        '0' : _pack(Data[:110, 4:], H),
+        '1' : _pack(Data[117:226, 4:], H),
+        '2' : _pack(Data[233:343, 4:], H),
+        '3' : _pack(Data[350:460, 4:], H),
+        '4' : _pack(Data[467:577, 4:], H),
+        '5' : _pack(Data[584:694, 4:], H),
+        '6' : _pack(Data[701:811, 4:], H)
+        }
     
-    train_nor_label, mu, std = _norLabel(train_un_label)
-    val_nor_label, _, _ = _norLabel(val_un_label)
-        
-    train_data = _pack(train_un_data, H)
-    val_data = _pack(val_un_data, H)
-    test_data = _pack(test_un_data, H)
+    train_label = {
+        '0' : Data[12:113, 4],
+        '1' : Data[129:229, 4],
+        '2' : Data[245:346, 4],
+        '3' : Data[362:463, 4],
+        '4' : Data[479:580, 4],
+        '5' : Data[596:697, 4],
+        '6' : Data[713:814, 4]    
+        }
     
-    return train_data, train_un_label, val_data, val_un_label, test_data, mu, std
+    val_data = {
+        '0' : _pack(Data[101:111, 4:], H),
+        '1' : _pack(Data[217:227, 4:], H),
+        '2' : _pack(Data[334:344, 4:], H),
+        '3' : _pack(Data[451:461, 4:], H),
+        '4' : _pack(Data[568:578, 4:], H),
+        '5' : _pack(Data[685:695, 4:], H),
+        '6' : _pack(Data[802:812, 4:], H)    
+        }
+    
+    val_label = {
+        '0' : Data[113, 4],
+        '1' : Data[229, 4],
+        '2' : Data[346, 4],
+        '3' : Data[463, 4],
+        '4' : Data[580, 4],
+        '5' : Data[697, 4],
+        '6' : Data[814, 4]    
+        }
+    
+    test_data = {
+        '0' : _pack(Data[104:114, 4:], H),
+        '1' : _pack(Data[220:230, 4:], H),
+        '2' : _pack(Data[337:347, 4:], H),
+        '3' : _pack(Data[454:464, 4:], H),
+        '4' : _pack(Data[571:581, 4:], H),
+        '5' : _pack(Data[688:698, 4:], H),
+        '6' : _pack(Data[805:815, 4:], H)    
+        }
+
+    return train_data, train_label, val_data, val_label, test_data
 
 #%% Test
 if __name__ == '__main__':
     fname = "training_data.csv"
-    train_data, train_nor_label, val_data, val_nor_label, test_data, mu, std = _PreProcess(fname)
+    train_data, train_nor_label, val_data, val_nor_label, test_data = _PreProcess(fname)
+
