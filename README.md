f2c573e56ac7e949d2c4562e19ae409c2fbc941d
Abner0627 <63701415+Abner0627@users.noreply.github.com>
2021-03-17 11:27:15 +0800

    3/17

--- README.md
+++ README.md
@@ -1,9 +1,69 @@
 # DSAI_HW1_ElectricityForecasting
 NCKU DSAI course homework
 
-[作業說明](https://www.dropbox.com/scl/fi/tx7md0teq0z4m3v20h5cp/DSAI-HW1-Electricity-Forecasting.paper?dl=0&rlkey=ajmzfqg0bjivr9bmcu8mqhv72)
+## 說明
+* 說明連結\
+[Dropbox paper](https://www.dropbox.com/scl/fi/tx7md0teq0z4m3v20h5cp/DSAI-HW1-Electricity-Forecasting.paper?dl=0&rlkey=ajmzfqg0bjivr9bmcu8mqhv72)
+* 目標\
+預測 2021/3/23 - 2021/3/29 台灣的電力備轉容量
 
+## 環境
+* python 3.6.4
+* Ubuntu 16.04.3 LTS
+
+## 使用方式
+1. 進入專案資料夾\
+`cd /d [path/to/this/project]`
+2. 安裝套件\
+`pip install -r requirements.txt`
+3. 執行\
 `python app.py --training training_data.csv --output submission.csv`
 
+## 資料來源
+* [台灣電力公司_過去電力供需資訊](https://data.gov.tw/dataset/19995)
+    * 2019/01/01 - 2021/01/31
+* [台灣電力公司_本年度每日尖峰備轉容量率](https://data.gov.tw/dataset/25850)
+    * 2021/01/01 - 當前
 
-input => 第幾周 星期幾
+## 前處理
+* 透過下列公式反推資料
+    * 備轉容量 = 系統運轉淨尖峰能力 - 系統瞬時尖峰負載
+    * 備轉容量率 = (備轉容量 ÷ 系統瞬時尖峰負載) × 100%
+
+![](/img/supply_load_remain.png)
+
+## 模型
+* train
+    2019/01/01 - 2021/02/28
+* test
+    2021/03/01 - 2021/03/14
+### MLP
+* 待補
+
+### ARIMA
+* 待補
+
+### Prophet
+[官方網站](https://facebook.github.io/prophet/)\
+\
+安裝步驟(python 3.6.4)
+* Windows 10
+    1. 安裝Microsoft C++ Build Tools \
+        [下載連結](https://visualstudio.microsoft.com/zh-hant/visual-cpp-build-tools/)
+    2. `pip install pystan==2.17.1.0`
+    3. `pip install fbprophet==0.6`
+
+* Ubuntu 16.04.3 LTS
+    1. 待補
+
+預測結果
+1. 直接預測 備轉容量(MW)
+    * RMSE = 142.0756
+
+![prophet_predict_remain](/img/prophet_predict_remain.png)
+
+2. 預測尖峰負載(MW) 並轉換為備轉容量(MW)
+    * 這邊假設備轉容量率固定為10%
+    * RMSE = 187.1624
+
+![prophet_predict_load2remain](/img/prophet_predict_load2remain.png)
--- preProcess_3.py
+++ preProcess_3.py
@@ -8,7 +8,7 @@ def _PreProcess(fname):
     #%% Path
     fpath = "./data"
     #fname = "training_data2.csv"
-    H = 10
+    H = 30
     #%% Functions
     def _pack(data, H):
         D = []
@@ -17,63 +17,19 @@ def _PreProcess(fname):
                 break
             else:
                 D.append(data[i:i+H, :])
-                
+
         D = np.array(D)
         return D
-    
+
     #%% Read csv
     Data = np.array(pd.read_csv(os.path.join(fpath, fname)))
-    
+
     # Data
-    train_data = {
-        '0' : _pack(Data[:110, 4:], H),
-        '1' : _pack(Data[117:226, 4:], H),
-        '2' : _pack(Data[233:343, 4:], H),
-        '3' : _pack(Data[350:460, 4:], H),
-        '4' : _pack(Data[467:577, 4:], H),
-        '5' : _pack(Data[584:694, 4:], H),
-        '6' : _pack(Data[701:811, 4:], H)
-        }
-    
-    train_label = {
-        '0' : Data[12:113, 4],
-        '1' : Data[129:229, 4],
-        '2' : Data[245:346, 4],
-        '3' : Data[362:463, 4],
-        '4' : Data[479:580, 4],
-        '5' : Data[596:697, 4],
-        '6' : Data[713:814, 4]    
-        }
-    
-    val_data = {
-        '0' : _pack(Data[101:111, 4:], H),
-        '1' : _pack(Data[217:227, 4:], H),
-        '2' : _pack(Data[334:344, 4:], H),
-        '3' : _pack(Data[451:461, 4:], H),
-        '4' : _pack(Data[568:578, 4:], H),
-        '5' : _pack(Data[685:695, 4:], H),
-        '6' : _pack(Data[802:812, 4:], H)    
-        }
-    
-    val_label = {
-        '0' : Data[113, 4],
-        '1' : Data[229, 4],
-        '2' : Data[346, 4],
-        '3' : Data[463, 4],
-        '4' : Data[580, 4],
-        '5' : Data[697, 4],
-        '6' : Data[814, 4]    
-        }
-    
-    test_data = {
-        '0' : _pack(Data[104:114, 4:], H),
-        '1' : _pack(Data[220:230, 4:], H),
-        '2' : _pack(Data[337:347, 4:], H),
-        '3' : _pack(Data[454:464, 4:], H),
-        '4' : _pack(Data[571:581, 4:], H),
-        '5' : _pack(Data[688:698, 4:], H),
-        '6' : _pack(Data[805:815, 4:], H)    
-        }
+    train_data = _pack(Data[:776, 5:8], H)
+    train_label = Data[30:790, 4]
+    val_data = _pack(Data[760:790, 5:8], H)
+    val_label = Data[790:804, 4]
+    test_data = _pack(Data[774:804, 5:8], H)
 
     return train_data, train_label, val_data, val_label, test_data
 
